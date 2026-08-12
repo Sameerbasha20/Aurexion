5145a4b23611572c2e7c69efb46c0d7683de2a84
kedhareswarivpd <kedhareswari@vpdtechnologies.com>
2026-08-12 17:45:01 +0530

    Administrator status

--- src/apps/portal/urls.py
+++ src/apps/portal/urls.py
@@ -1,10 +1,3 @@
 from django.urls import path
-from apps.portal.views import PortalLoginView, PortalLogoutView, DashboardView
 
-app_name = 'portal'
-
-urlpatterns = [
-    path('', DashboardView.as_view(), name='dashboard'),
-    path('login/', PortalLoginView.as_view(), name='login'),
-    path('logout/', PortalLogoutView.as_view(), name='logout'),
-]
+urlpatterns = []
